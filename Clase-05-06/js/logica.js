161c5aaffb05b0cd9a2f488f495b6de06c68fe1b
Star <terobastian@gmail.com>
2024-06-12 22:45:54 -0500

    Armando la logica y las vistas de selector y la pagina main

--- Clase-05-06/js/logica.js
+++ Clase-05-06/js/logica.js
@@ -1,1 +1,23 @@
 console.log("Logica conectada.");
+
+const user = "Admin";
+const password = "123";
+
+function buttonTest() {
+
+let usu = document.getElementById("inputUser").value;
+let pass = document.getElementById("inputPassword").value;
+
+    if (usu == "" || pass == "") {
+        alert("Rellene las casillas por favor.");
+    } else {
+        if (user == usu && password == pass) {
+            window.open(`calculadora.html`);
+            console.log(`Sesion iniciada.`);
+            document.getElementById("wc").innerHTML(`Bienvenido ${user}`);
+        } else {
+            console.log(`Error de credenciales.`);
+        }
+    }
+
+}

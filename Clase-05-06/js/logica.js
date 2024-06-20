527fb8119ff3c955181fc2133303537a021536fe
Star <terobastian@gmail.com>
2024-06-20 18:53:50 -0500

    Arreglos, local storage y complementando selectores.html

--- Clase-05-06/js/logica.js
+++ Clase-05-06/js/logica.js
@@ -3,6 +3,8 @@ console.log("Logica conectada.");
 const user = "Admin";
 const password = "123";
 
+const frutas= [`Manzana`, `Pera`, `Sandia`];
+
 function buttonTest() {
 
 let usu = document.getElementById("inputUser").value;
@@ -20,4 +22,56 @@ let pass = document.getElementById("inputPassword").value;
         }
     }
 
+}
+
+
+function imprimirArray(){
+    
+    let posicion=document.getElementById(`array`).value;
+    console.log(localStorage.getItem(`arregloFrutas`));
+    
+    if(posicion>=frutas.length){
+        console.log("Error.")
+    }else{
+    console.log(frutas[posicion]);
+    limpiar();
+}
+
+}
+
+
+function limpiar(){
+    document.getElementById(`array`).value=" ";  
+    document.getElementById(`array`).focus();
+}
+
+
+function agregarFruta(){
+
+    let fruitName=document.getElementById(`nombreFruta`).value.toLocaleUpperCase();
+    fruitName=fruitName.trim();
+
+    if(fruitName.trim()!==""){
+        frutas.push(fruitName);
+
+        saveStorage(frutas);
+
+        console.log(`${fruitName}`);
+        document.getElementById(`nombreFruta`).focus;
+    }else{
+        console.log("Nombre invalido.");
+    }
+}
+
+
+function eliminarFruta(){
+    let posicion=document.getElementById('array').value;
+    frutas.splice(posicion,1);
+    console.log(frutas);
+}
+
+
+function saveStorage(arreglo){
+    localStorage.setItem(`arregloFrutas`, arreglo);
+    console.log("Guardado en storage");
 }

223fc461d21f130618f8e5ee6dc8dc927192bbdc
Star <terobastian@gmail.com>
2024-07-04 10:32:01 -0500

    Primer contacto con JSON y armando interfaces

---
+++ Clase-05-06/js/personas.js
@@ -0,0 +1,81 @@
+let dataPersonasJSON=[
+
+        {id:1, nombre:"Oscar", edad:20,},
+
+        {
+            id:2, 
+            nombre:"Maria", 
+            edad:45,
+            deportes:[
+                {
+                    deporte:'tennis', 
+                    dias:['lunes','miercoles']
+                },
+                {
+                    deporte:futbol,
+                    dias:['jueves','viernes','domingo']
+                }
+            ]
+        }];
+
+
+let dataPlato=[
+    
+    {
+        "nombre_plato":"pasta",
+        "precio":40.868,
+        "chef":"Mario",
+        "ingredientes":[
+            {
+                "nombre":"pasta_cruda",
+                "cantidad":"500g",
+                "precio":4.340
+            },
+            {
+                "nombre":"salsa_tomate",
+                "cantidad":"200g",
+                "precio":5.210
+            },
+            {
+                "nombre":"carne_molida",
+                "cantidad":"700g",
+                "precio":18.186
+            }
+            
+        ],
+        "acompañantes":[
+            {
+                "nombre":"Pan",
+                "cantidad":2
+            },
+            {
+                "bebidas":[{
+                    "bebidasFrias":[
+                        {
+                            "nombre":"cocaCola",
+                            "precio":1800
+                        },
+                        {
+                            "nombre":"jugoHit",
+                            "precio":1600
+                        },
+                        {
+                            "nombre":"vino",
+                            "precio":18000
+                        }]
+                },
+                {
+                    "bebidasCalientes":[
+                        {
+                            "nombre":"cafe",
+                            "precio":700
+                        },
+                        {
+                            "nombre":"mate",
+                            "precio":1000
+                        }]
+                }]
+            }]
+    }]
+
+        
